c0ee7cd3dd10535fccd22cc2848eb6ba22988d26
JozefStodulski <jozefstodulski@gmail.com>
2015-02-01 15:35:14 +0000

    added ask properties
    and setters

--- ProjectPlanner/src/projectplanner/Task.java
+++ ProjectPlanner/src/projectplanner/Task.java
@@ -4,11 +4,59 @@
  * and open the template in the editor.
  */
 package projectplanner;
-
+import java.util.Date;
+import java.util.*;
 /**
  *
  * @author Samsung
  */
 public class Task {
+    private int taskID;
+    private String taskName;
+    private Date EST;
+    private int duration;
+    private Date EET;
+    private int LST;
+    private int floatTime;
+    private Date LET;
+    private int eventID;
+    private List<Task> dependancies;
     
+    public Task(int taskID, String taskName, Date EST, int duration)
+    {
+        setTaskID(taskID);
+        setTaskName(taskName);
+        setEST(EST);
+        setDuration(duration);
+    }
+    
+    public void setTaskID(int ID)
+    {
+        taskID = ID;
+    }
+    
+    public void setTaskName(String name)
+    {
+        taskName = name;
+    }
+    
+    public void setEST(Date time)
+    {
+        EST = time;
+    }
+    
+    public void setDuration(int time)
+    {
+        duration = time;
+    }
+    
+    public void addDependency(Task dependency)
+    {
+        dependancies.add(dependency);
+    }
+    
+    public List<Task> getDependencies()
+    {
+        return dependancies;
+    }
 }
